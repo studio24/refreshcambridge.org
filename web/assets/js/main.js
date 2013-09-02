54f585e64a5ec0e4489b4a36c0d19342f8288541
Scott Mokler <scott.mokler@studio24.net>
2013-09-02 15:29:56 +0100

    adding changes

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -16,19 +16,22 @@ function initialize() {
   var mapOptions = {
     zoom: 16,
     center: new google.maps.LatLng(52.2074008,0.1223454),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    
+    mapTypeControl: false,
+
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-var image = '/assets/img/pin.png';
-
+var icon = "/assets/img/marker.png";
 
   var myLatLng = new google.maps.LatLng(52.2074008,0.1223454);
   var marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
-      icon: image
+      icon: icon
+
   });
 
 
