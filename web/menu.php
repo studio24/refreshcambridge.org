74c9f0500fdd1575a4ce2f200b3fbd229e988194
Simon R Jones <simon@studio24.net>
2014-01-18 21:40:13 +0000

    Updating menu links

--- web/menu.php
+++
@@ -1,5 +0,0 @@
-						<ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/about.php">About</a></li>
-                            <li><a href="/pastevents.php">Past Events</a></li>
-                        </ul>
