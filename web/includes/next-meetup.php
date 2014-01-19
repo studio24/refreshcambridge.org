a3673083f4e446de04f5254ec7c207b6c02d60a2
Simon R Jones <simon@studio24.net>
2014-01-19 00:28:47 +0000

    Removing old map URL code (not used)

--- web/includes/next-meetup.php
+++ web/includes/next-meetup.php
@@ -1,12 +1,5 @@
-<?php 
-// Cambridge BrewHouse
-$mapUrl = "https://www.google.com/maps/preview#!data=!1m4!1m3!1d2289!2d0.1223454!3d52.2074008!4m30!3m15!1m0!1m4!2s0x47d87095d95bb29b%3A0xade5e50e00eec178!3m2!3d52.2074008!4d0.1223454!3m8!1m3!1d24417542!2d-95.677068!3d37.0625!3m2!1i1440!2i719!4f13.1!5m11!1m10!1s52.2074008%2C0.1223454!4m8!1m3!1d24417542!2d-95.677068!3d37.0625!3m2!1i1440!2i719!4f13.1!7m1!3b1&fid=0";
 
-// Devonshire Arms
-//$mapUrl = "https://maps.google.co.uk/maps?q=Devonshire+Arms,+Devonshire+Road,+Cambridge&hl=en&ll=52.19894,0.139067&spn=0.011008,0.020192&sll=52.37288,0.007274&sspn=1.403536,2.584534&oq=devonshire+arms,+&hq=Devonshire+Arms,&hnear=Devonshire+Rd,+Cambridge,+United+Kingdom&t=m&z=16&iwloc=A";
-
-?>
-				<aside id="nextmeetup" class="twoend column end darkGreen">
+		<aside id="nextmeetup" class="twoend column end darkGreen">
                     <p>Next Meetup</p>
                     <h1>5<sup>th</sup> Feb</h1>
                     <div class="clear"></div>
