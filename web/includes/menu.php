7af6556fab07cf0fb6f1c03f00a65bf3750ef46e
Simon R Jones <simon@studio24.net>
2014-01-19 00:29:42 +0000

    Updating clean URLs to remove *.php

--- web/includes/menu.php
+++ web/includes/menu.php
@@ -1,5 +1,5 @@
 			<ul>
                             <li><a href="/">Home</a></li>
                             <li><a href="/about">About</a></li>
-                            <li><a href="/past-events">Past Events</a></li>
+                            <li><a href="/past-events/">Past Events</a></li>
                         </ul>
